abb687d5f793ee040f8983c3589f2fb7174b291b
jgrissom <jgrissom91@gmail.com>
2024-06-13 23:22:01 -0500

    [01-15] Save Target

--- code/main.py
+++ code/main.py
@@ -9,22 +9,34 @@ from async_button import Pushbutton
 def handle_press():
     print('press')
     led.on()
+    my_gps.saved_lat = my_gps.latitude
+    my_gps.saved_lon = my_gps.longitude
+    my_gps.saved_sat = my_gps.satellites_in_use
 
 def handle_long():
     print('long')
     led.off()
+    my_gps.saved_lat = None
+    my_gps.saved_lon = None
+    my_gps.saved_sat = 0
 
 async def main():
     while True:
-        length = gps_module.any()
-        if length > 0:
-            b = gps_module.read(length)
-            for x in b:
-                msg = my_gps.update(chr(x))
-        latitude = my_gps.convert(my_gps.latitude)
-        longitude = my_gps.convert(my_gps.longitude)
-        satellites = my_gps.satellites_in_use
-        
+        if (my_gps.saved_lat == None or my_gps.saved_lon == None):
+            length = gps_module.any()
+            if length > 0:
+                b = gps_module.read(length)
+                for x in b:
+                    msg = my_gps.update(chr(x))
+            latitude = my_gps.convert(my_gps.latitude)
+            longitude = my_gps.convert(my_gps.longitude)
+            satellites = my_gps.satellites_in_use
+            target = "---NO  TARGET---"
+        else:
+            latitude = my_gps.convert(my_gps.saved_lat)
+            longitude = my_gps.convert(my_gps.saved_lon)
+            satellites = my_gps.saved_sat
+            target = "--TARGET SAVED--"
         oled.fill(0)
         if (latitude == None or longitude == None):
             oled.text("No Data", 0, 0)
@@ -32,6 +44,7 @@ async def main():
             oled.text('Lat:'+ latitude, 0, 0)
             oled.text('Lng:'+ longitude, 0, 11)
             oled.text('Satellites:' + str(satellites), 0, 22)
+            oled.text(target, 0, 44)
         oled.show()
         
         await asyncio.sleep(.5)
@@ -53,4 +66,6 @@ if __name__ == '__main__':
         asyncio.run(main())
     finally:
         led.off()
+        oled.fill(0)
+        oled.show()
         print('goodbye')
