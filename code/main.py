352ecdb8b064da61d98ca7fae8be5cbae8451134
jgrissom <jgrissom91@gmail.com>
2024-06-13 22:42:12 -0500

    [01-09] Asyncronous Infinite Loop

--- code/main.py
+++ code/main.py
@@ -1,9 +1,17 @@
 # main.py
 
-import led
+from machine import Pin
+import asyncio
+
+async def main():
+    while True:
+        led.value(btn.value())
+        await asyncio.sleep(.1)
 
 if __name__ == '__main__':
     try:
-        led.blink(7)
+        btn = Pin(8, Pin.IN, Pin.PULL_UP)
+        led = Pin(9, Pin.OUT)
+        asyncio.run(main())
     finally:
         print('goodbye')
