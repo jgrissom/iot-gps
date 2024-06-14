43e7d3ad2a5db79ebb2705a3181f14724ec22d68
jgrissom <jgrissom91@gmail.com>
2024-06-13 22:45:02 -0500

    [01-10] Momentary Switch Driven LED - async

--- code/main.py
+++ code/main.py
@@ -2,16 +2,23 @@
 
 from machine import Pin
 import asyncio
+from async_button import Pushbutton
+
+def handle_press():
+    print('press')
+    led.on()
 
 async def main():
     while True:
-        led.value(btn.value())
         await asyncio.sleep(.1)
 
 if __name__ == '__main__':
     try:
-        btn = Pin(8, Pin.IN, Pin.PULL_UP)
+        btn = Pushbutton( Pin(8, Pin.IN, Pin.PULL_UP) )
+        btn.press_func(handle_press)
+        
         led = Pin(9, Pin.OUT)
         asyncio.run(main())
     finally:
+        led.off()
         print('goodbye')
